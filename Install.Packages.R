a183b750c6a3aecf1adebc07aafcbb3bc562c91d
Diego Maximiliano Martínez Rosales <44356546+diegommartinezr@users.noreply.github.com>
2020-04-21 21:44:01 +0200

    Update Install.Packages.R

--- Install.Packages.R
+++ Install.Packages.R
@@ -1,1 +1,9 @@
 install.packages("devtools")
+install.packages("readr")
+install.packages("ggplot2")
+install.packages("tidyverse")
+
+library(devtools)
+
+devtools::install_github("haozhu233/kableExtra")
+devtools::install_github("tidyverse/readr")
